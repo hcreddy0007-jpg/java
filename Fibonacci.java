a3d48a351225e6bcb77d55a5eeee8c3ffbf44016
hcreddy0007-jpg <hcreddy0007@gmail.com>
2026-02-10 11:39:51 +0530

    First Changes

---
+++ Fibonacci.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Fibonacci{
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        
+        System.out.print("Enter number of terms: ");
+        int n = s.nextInt();
+
+        int first = 0, second = 1;
+
+        System.out.println("Fibonacci Series:");
+
+        for(int i = 1; i <= n; i++) {
+            System.out.print(first + " ");
+
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+
+        s.close();
+    }
+}
