a2d66673f6e52a1df2085109a2200c49a187a5e7
hcreddy0007-jpg <hcreddy0007@gmail.com>
2026-02-10 11:26:37 +0530

    First changes

---
+++ prime.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class prime{
+    public static void main(String args[]) {
+        int num;
+        Scanner s = new Scanner(System.in);
+        boolean isprime = true;
+        num = s.nextInt() ;
+        for(int i = 2; i<num; i++){
+            if((num%i)==0){
+                isprime = false;
+                break;
+            }
+        }
+        if(isprime) System.out.println("prime");
+        else System.out.println("not prime");
+        
+    }
+}
---
+++ whileodd.java
@@ -0,0 +1,9 @@
+class whileodd{
+    public static void main(String[] args){
+        int count = 1;
+        while(count < 11){
+            System.out.println("odd number" + ((2*count)-1));
+            count++;
+        }
+    }
+}
