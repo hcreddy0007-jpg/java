e67fe5cd57fedab91eaa068632a6f5ce2b65eec0
hcreddy0007-jpg <hcreddy0007@gmail.com>
2026-02-10 11:07:19 +0530

    First Changes

---
+++ dowhile.java
@@ -0,0 +1,12 @@
+class dowhile{
+    public static void main(String[] args){
+        int count = 1;
+        do{
+            // even number up to 11 numbers
+            System.out.println("EVEN IS " + 2*count);
+            count++;
+
+        }while(count<11);
+    }
+    
+}
---
+++ loop.java
@@ -0,0 +1,7 @@
+class loop{
+    public static void main(String[] args) {
+        for(int i = 1; i<11; i++) {
+            System.out.println("Count i " +i);
+        }
+    }
+}
